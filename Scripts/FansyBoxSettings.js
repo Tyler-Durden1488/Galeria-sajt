d8df0937afee94dc11858af7c93f542865a521bd
Vovchik731 <neojew731@gmail.com>
2024-11-21 10:19:38 +0100

    Imaga functions

---
+++ Scripts/FansyBoxSettings.js
@@ -0,0 +1,38 @@
+Fancybox.bind('[data-fancybox', {
+
+
+    Toolbar: {
+        display: {
+            left: [],
+            middle: [],
+            right: ["close"],
+        },
+    },
+
+    Images: {
+        Panzoom: {
+            panMode: "mousemove",
+            mouseMoveFactor: 1.1,
+            mouseMoveFriction: 0.12
+        },
+    },
+
+    Toolbar: {
+        display: {
+            left: ["infobar"],
+            middle: [
+                "zoomIn",
+                "zoomOut",
+                "toggle1to1",
+                "rotateCCW",
+                "rotateCW",
+                "flipX",
+                "flipY",
+            ],
+            right: ["fullscreen","download", "close"],
+        },
+    },
+    
+
+
+});    
